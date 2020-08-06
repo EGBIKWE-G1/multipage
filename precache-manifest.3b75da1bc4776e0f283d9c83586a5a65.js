5cdb1d5f51f9e233bd6e566649ce35433e5a69f7
GODWIN-G1 <egbikwechinazom@gmail.com>
2020-08-06 10:23:28 +0100

    Updates

---
+++ precache-manifest.3b75da1bc4776e0f283d9c83586a5a65.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9de204b26ca5309988ee11ba54d7a6d",
+    "url": "/multipage/index.html"
+  },
+  {
+    "revision": "3825c804d71c84f39133",
+    "url": "/multipage/static/css/main.fd30aae4.chunk.css"
+  },
+  {
+    "revision": "11099fbe471a067fc928",
+    "url": "/multipage/static/js/2.c1c39951.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/multipage/static/js/2.c1c39951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3825c804d71c84f39133",
+    "url": "/multipage/static/js/main.0eea3370.chunk.js"
+  },
+  {
+    "revision": "2af42a169dd3a07aede7",
+    "url": "/multipage/static/js/runtime-main.d06c57f8.js"
+  },
+  {
+    "revision": "9722cdcb17486338ceb298fd266a9cc4",
+    "url": "/multipage/static/media/Appic.9722cdcb.png"
+  },
+  {
+    "revision": "41d209953cf220cb6fafd1c0f8978150",
+    "url": "/multipage/static/media/Brand.41d20995.png"
+  },
+  {
+    "revision": "1ff45dbab3a3711e7bb545666446a86f",
+    "url": "/multipage/static/media/Emergency.1ff45dba.png"
+  },
+  {
+    "revision": "79e4fdedef1a809a4c3a1c399a75237c",
+    "url": "/multipage/static/media/Madagascar.79e4fded.png"
+  },
+  {
+    "revision": "dc69dba73217196f55eea451c9173d80",
+    "url": "/multipage/static/media/doublepic.dc69dba7.png"
+  },
+  {
+    "revision": "aa7f1e85755a342a97ef7c3b28a6ff1b",
+    "url": "/multipage/static/media/slider.aa7f1e85.png"
+  }
+]);
